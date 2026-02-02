637c7232ad56311dee58230a4707f5ab6f3a760d
Ibrahim Maâzou Djahadi <ibrahimdjahadi46@gmail.com>
2026-02-02 15:30:54 +0200

    feat: add a simple http server in Express

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors({ origin: "*" }));
+app.use(morgan("combined"));
+
+app.get("/hello", (req, res) => {
+  let i = 1e7;
+  while (i > 0) {
+    i--;
+  }
+
+  return res.status(200).json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
